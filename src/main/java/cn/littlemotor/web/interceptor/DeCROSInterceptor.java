3a707f4ed5cfd205130a954c25301b9e601f9a45
littlemotor <479217391@qq.com>
2019-01-24 22:30:38 +0800

    测试环境中防止cros警告

---
+++ src/main/java/cn/littlemotor/web/interceptor/DeCROSInterceptor.java
@@ -0,0 +1,30 @@
+package cn.littlemotor.web.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 在测试中由于浏览器会出现CROS提示，需要用这个浏览器在头文件中做一些修改
+ */
+public class DeCROSInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        response.addHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
