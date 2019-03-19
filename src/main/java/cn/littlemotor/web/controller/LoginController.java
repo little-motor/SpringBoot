1c098aec65010d20a30286e0ee5e3cf22ff4ee0b
littlemotor <479217391@qq.com>
2019-03-19 23:29:04 +0800

    实现登陆基础方法

--- src/main/java/cn/littlemotor/web/controller/LoginController.java
+++ src/main/java/cn/littlemotor/web/controller/LoginController.java
@@ -1,7 +1,11 @@
 package cn.littlemotor.web.controller;
 
+import cn.littlemotor.web.model.service.user.UserLogin;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * 登陆控制器，负责检验用户的登陆状态
@@ -10,9 +14,21 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 public class LoginController {
+
+    //打开登陆页面
     @GetMapping(path = "/login")
     public String login(){
 
         return "login.html";
+    }
+
+    //用户登陆
+    @ResponseBody
+    @PostMapping(path = "/login")
+    public boolean login(@RequestBody UserLogin userLogin){
+        System.out.println("password: " + userLogin.getPassword());
+        System.out.println("remember: " + userLogin.getRememberMe());
+        System.out.println("email: " + userLogin.getEmail());
+        return true;
     }
 }
