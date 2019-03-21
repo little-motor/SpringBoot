8adbad55e06a7a3009d6d6b4547dd0b400ce6770
littlemotor <479217391@qq.com>
2019-03-21 21:49:07 +0800

    主页可以根据session拿到数据

--- src/main/java/cn/littlemotor/web/controller/HomeController.java
+++ src/main/java/cn/littlemotor/web/controller/HomeController.java
@@ -1,20 +1,30 @@
 package cn.littlemotor.web.controller;
 
+import cn.littlemotor.web.model.service.user.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
 
+    //考虑有无session的情况，required改为false
     @GetMapping(path = "/")
-    public String home(){
+    public String homeWithSession(HttpSession httpSession){
         System.out.println("home");
-        return "forward:/home.html";
+        if(httpSession.getAttribute("user") != null){
+            System.out.println(((User)httpSession.getAttribute("user")).getName());
+        }
+        return "/home.html";
     }
 
     @GetMapping(path = "/home")
-    public String home2(){
-        System.out.println("home");
+    public String homeRedirect(){
+        System.out.println("home2");
         return "redirect:/";
     }
+
+
 }
