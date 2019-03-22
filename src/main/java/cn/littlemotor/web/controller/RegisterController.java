1a189561990711a0cb3d958adff13981258192c1
littlemotor <479217391@qq.com>
2019-03-22 23:07:22 +0800

    改用restcontroller

--- src/main/java/cn/littlemotor/web/controller/RegisterController.java
+++ src/main/java/cn/littlemotor/web/controller/RegisterController.java
@@ -3,33 +3,39 @@ package cn.littlemotor.web.controller;
 import cn.littlemotor.web.model.dao.UserDao;
 import cn.littlemotor.web.model.service.user.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 用户注册控制器
  * @author littlemotor
  * @date 19.1.26
  */
-@Controller
+@RestController
 public class RegisterController {
 
     @Autowired
     UserDao userDao = null;
 
     /**
-     * 此处收到的json结构不是普通的结构无法直接实例化为user对象，需要自己定义httpmessageconverter
-     * 在下次重构过程中进行修改
+     * responseEntity可以封装对象，添加http头，以及响应码
      */
-    @ResponseBody
     @PostMapping(path = "/register")
-    public boolean register(@RequestBody User user) {
+    public ResponseEntity<String> register(@RequestBody User user) {
+        HttpHeaders httpHeaders = new HttpHeaders();
         try {
             userDao.setUser(user);
+            httpHeaders.add("registed", "success");
         }catch (Exception e){
+//            System.out.println("输出错误：" + e.toString());
+            throw e;
+
         }
-        return true;
+        return new ResponseEntity<>(httpHeaders,HttpStatus.CREATED);
     }
+
 }
