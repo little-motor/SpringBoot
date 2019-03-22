f35510537fbb134077aec867260a978f6e4e44b2
littlemotor <479217391@qq.com>
2019-03-22 23:05:51 +0800

    改用restcontroller

--- src/main/java/cn/littlemotor/web/controller/HomeController.java
+++ src/main/java/cn/littlemotor/web/controller/HomeController.java
@@ -1,23 +1,24 @@
 package cn.littlemotor.web.controller;
 
 import cn.littlemotor.web.model.service.user.User;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
 
-@Controller
+@RestController
 public class HomeController {
 
     //考虑有无session的情况，required改为false
     @GetMapping(path = "/")
-    public String homeWithSession(HttpSession httpSession){
+    public ModelAndView homeWithSession(HttpSession httpSession){
+        ModelAndView modelAndView = new ModelAndView("home.html");
         System.out.println("home");
         if(httpSession.getAttribute("user") != null){
             System.out.println(((User)httpSession.getAttribute("user")).getName());
         }
-        return "/home.html";
+        return modelAndView;
     }
 
     @GetMapping(path = "/home")
