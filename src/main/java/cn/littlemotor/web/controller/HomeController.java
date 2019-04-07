abae36b0f076d9911c446bbddf3860f9bd53bff4
littlemotor <479217391@qq.com>
2019-04-08 00:04:26 +0800

    以后在需要post的页面head里面加入csrf字段，用于防止csrf攻击，注意获取csrfToken的方式是从httpRequest里面而不是在session里面

--- src/main/java/cn/littlemotor/web/controller/HomeController.java
+++ src/main/java/cn/littlemotor/web/controller/HomeController.java
@@ -1,23 +1,24 @@
 package cn.littlemotor.web.controller;
 
-import cn.littlemotor.web.model.service.user.User;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 public class HomeController {
 
     //考虑有无session的情况，required改为false
     @GetMapping(path = "/")
-    public ModelAndView homeWithSession(HttpSession httpSession){
+    public ModelAndView homeWithSession(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse){
+        CsrfToken csrfToken = (CsrfToken) (httpServletRequest.getAttribute("org.springframework.security.web.csrf.CsrfToken"));
+        httpServletResponse.addHeader("X-CSRF-TOKEN", csrfToken.getToken());
+        //System.out.println(csrfToken);
         ModelAndView modelAndView = new ModelAndView("home.html");
         System.out.println("home");
-        if(httpSession.getAttribute("user") != null){
-            System.out.println(((User)httpSession.getAttribute("user")).getName());
-        }
         return modelAndView;
     }
 
@@ -27,5 +28,21 @@ public class HomeController {
         return "redirect:/";
     }
 
+//    /**
+//     * responseEntity可以封装对象，添加http头，以及响应码
+//     */
+//    @PostMapping(path = "/register")
+//    public ResponseEntity<String> register(@RequestBody User user) {
+//        HttpHeaders httpHeaders = new HttpHeaders();
+//        try {
+//            userDao.setUser(user);
+//            httpHeaders.add("registed", "success");
+//        }catch (Exception e){
+////            System.out.println("输出错误：" + e.toString());
+//            throw e;
+//
+//        }
+//        return new ResponseEntity<>(httpHeaders, HttpStatus.CREATED);
+//    }
 
 }
