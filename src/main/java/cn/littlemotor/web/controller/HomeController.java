2b8d8829e823a86bce753325da8ecfbce36d20bb
littlemotor <479217391@qq.com>
2019-04-10 22:11:49 +0800

    cookie的处理交给controllerAdvice单独处理

--- src/main/java/cn/littlemotor/web/controller/HomeController.java
+++ src/main/java/cn/littlemotor/web/controller/HomeController.java
@@ -1,28 +1,15 @@
 package cn.littlemotor.web.controller;
 
-import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @RestController
 public class HomeController {
 
     //考虑有无session的情况，required改为false
     @GetMapping(path = "/")
-    public ModelAndView homeWithSession(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse){
-        /** 获取token的方式有多种可以通过request的getAttribute也可以通过session
-        * 他们的可以非别为"org.springframework.security.web.csrf.CsrfToken"、"_csrf"和"org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository.CSRF_TOKEN"
-        * 推荐使用servlet获取，session不太稳定
-        */
-        CsrfToken csrfToken = (CsrfToken) httpServletRequest.getAttribute("org.springframework.security.web.csrf.CsrfToken");
-        //System.out.println(csrfToken.getToken());
-        Cookie csrfCookie = new Cookie("X-CSRF-TOKEN", csrfToken.getToken());
-        httpServletResponse.addCookie(csrfCookie);
+    public ModelAndView homeWithSession(){
         ModelAndView modelAndView = new ModelAndView("home.html");
         System.out.println("home");
         return modelAndView;
@@ -31,7 +18,7 @@ public class HomeController {
     @GetMapping(path = "/home")
     public ModelAndView homeRedirect(){
         System.out.println("home2");
-        ModelAndView modelAndView = new ModelAndView("home.html");
+        ModelAndView modelAndView = new ModelAndView("redirect:/");
         return modelAndView;
     }
 
