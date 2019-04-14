2968c4264424e75573122d05cf018ee7accdf73b
littlemotor <479217391@qq.com>
2019-04-14 22:07:46 +0800

    增加注销后删除cookie的操作

---
+++ src/main/java/cn/littlemotor/web/security/CustomLogoutSuccessHandler.java
@@ -0,0 +1,35 @@
+package cn.littlemotor.web.security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 定制注销成功后的一些后续操作，主要是删除cookie
+ *
+ * @author littlemotor
+ * @date 19.4.14
+ */
+public class CustomLogoutSuccessHandler implements LogoutSuccessHandler {
+
+    public CustomLogoutSuccessHandler(){
+
+    }
+
+    //负责推出后对cookie进行删除操作
+    @Override
+    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        Cookie[] cookies = request.getCookies();
+        for(Cookie cookie : cookies){
+            cookie.setValue(null);
+            cookie.setMaxAge(0);
+            response.addCookie(cookie);
+        }
+
+    }
+}
