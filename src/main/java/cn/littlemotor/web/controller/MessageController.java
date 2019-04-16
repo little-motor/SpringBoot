3872ea85f7b3b92ddf291e216e67e1a436fedb8c
littlemotor <479217391@qq.com>
2019-04-16 23:31:28 +0800

    增加处理post的控制器

--- src/main/java/cn/littlemotor/web/controller/MessageController.java
+++ src/main/java/cn/littlemotor/web/controller/MessageController.java
@@ -1,6 +1,14 @@
 package cn.littlemotor.web.controller;
 
+import cn.littlemotor.web.model.dao.MessageDao;
+import cn.littlemotor.web.model.service.content.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -13,10 +21,25 @@ import org.springframework.web.servlet.ModelAndView;
 @RestController
 public class MessageController {
 
-    @GetMapping(value = "/message")
-    public ModelAndView getMessage(){
+    @Autowired
+    MessageDao messageDao = null;
 
+    @GetMapping(path = "/message")
+    public ModelAndView getMessage(){
         ModelAndView modelAndView = new ModelAndView("message.html");
         return modelAndView;
+    }
+
+    @PostMapping(path = "/message")
+    public ResponseEntity<String> postMessage(@RequestBody Message message){
+        try{
+            messageDao.insertMessage(message);
+        } catch (Exception e){
+            System.out.println(e);
+            throw e;
+        }
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set("Message","发送成功");
+        return new ResponseEntity<>(httpHeaders, HttpStatus.CREATED);
     }
 }
