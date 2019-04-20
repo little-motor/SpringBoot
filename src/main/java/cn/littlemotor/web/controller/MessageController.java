723d89c844490fd9587858eceeeeeb31073c00e1
littlemotor <479217391@qq.com>
2019-04-20 23:11:18 +0800

    messageController增加返回message方法

--- src/main/java/cn/littlemotor/web/controller/MessageController.java
+++ src/main/java/cn/littlemotor/web/controller/MessageController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 /**
  * 用于处理用户发送消息的控制器
  * @author littlemotor
@@ -24,17 +26,40 @@ public class MessageController {
     @Autowired
     MessageDao messageDao = null;
 
+    /**
+     * 返回message页面
+     * @return
+     */
     @GetMapping(path = "/message")
     public ModelAndView getMessage(){
         ModelAndView modelAndView = new ModelAndView("message.html");
         return modelAndView;
     }
 
+    @GetMapping(path = "/message/getData")
+    public void getMessageData(){
+        try {
+            List<Message> messageList = messageDao.getMessage(54);
+        } catch (Exception e){
+            System.out.println("getMessageData wrong");
+            throw e;
+        }
+
+        ResponseEntity<String> responseEntity = new ResponseEntity<>();
+
+    }
+
+
+    /**
+     * 此处message的属性有messageId、用户id、userId、message、
+     * likeNum、评论列表comments
+     * @param message
+     * @return
+     */
     @PostMapping(path = "/message")
     public ResponseEntity<String> postMessage(@RequestBody Message message){
         try{
             messageDao.insertMessage(message);
-            messageDao.insertUserMessage(message);
         } catch (Exception e){
             System.out.println(e);
             throw e;
