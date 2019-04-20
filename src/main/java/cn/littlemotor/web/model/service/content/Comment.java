6b30f8f065b767bb1f2978662a81355e34743467
littlemotor <479217391@qq.com>
2019-04-20 23:12:07 +0800

    增加评论Comment pojo

---
+++ src/main/java/cn/littlemotor/web/model/service/content/Comment.java
@@ -0,0 +1,31 @@
+package cn.littlemotor.web.model.service.content;
+
+public class Comment {
+    private int commentId;
+    private int messageId;
+    private String commentData = null;
+
+    public int getCommentId() {
+        return commentId;
+    }
+
+    public void setCommentId(int commentId) {
+        this.commentId = commentId;
+    }
+
+    public int getMessageId() {
+        return messageId;
+    }
+
+    public void setMessageId(int messageId) {
+        this.messageId = messageId;
+    }
+
+    public String getCommentData() {
+        return commentData;
+    }
+
+    public void setCommentData(String commentData) {
+        this.commentData = commentData;
+    }
+}
