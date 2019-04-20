a9cd30893648c93c0177bfaebe4d778d082862ff
littlemotor <479217391@qq.com>
2019-04-20 23:12:18 +0800

    增加like表pojo

---
+++ src/main/java/cn/littlemotor/web/model/service/content/Like.java
@@ -0,0 +1,31 @@
+package cn.littlemotor.web.model.service.content;
+
+public class Like {
+    private int likeId;
+    private int messageId;
+    private int likeNum;
+
+    public int getLikeId() {
+        return likeId;
+    }
+
+    public void setLikeId(int likeId) {
+        this.likeId = likeId;
+    }
+
+    public int getMessageId() {
+        return messageId;
+    }
+
+    public void setMessageId(int messageId) {
+        this.messageId = messageId;
+    }
+
+    public int getLikeNum() {
+        return likeNum;
+    }
+
+    public void setLikeNum(int likeNum) {
+        this.likeNum = likeNum;
+    }
+}
