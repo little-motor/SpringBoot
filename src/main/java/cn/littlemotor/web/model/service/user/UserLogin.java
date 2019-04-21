2075b6d7b6053bf1ffa0576df78844329839b872
littlemotor <479217391@qq.com>
2019-04-21 23:13:19 +0800

    no message

--- src/main/java/cn/littlemotor/web/model/service/user/UserLogin.java
+++ src/main/java/cn/littlemotor/web/model/service/user/UserLogin.java
@@ -15,18 +15,18 @@ public class UserLogin extends User{
     public UserLogin() {
     }
 
-    //验证用户登陆时输入的账号和数据库中的信息是否匹配
-    public boolean validateUser(User user){
-        if(user == null){
-            return false;
-        }
-        //注意此处比较字符串应该用equal方法而不是==
-        //==方法比较两个对象的地址，肯定为false
-        if(!user.getPassword().equals(this.getPassword())){
-            return false;
-        }
-        return true;
-    }
+//    //验证用户登陆时输入的账号和数据库中的信息是否匹配
+//    public boolean validateUser(User user){
+//        if(user == null){
+//            return false;
+//        }
+//        //注意此处比较字符串应该用equal方法而不是==
+//        //==方法比较两个对象的地址，肯定为false
+//        if(!user.getPassword().equals(this.getPassword())){
+//            return false;
+//        }
+//        return true;
+//    }
 
     //因为从数据库中取出来之后再setPassword的时候是不需要加密的
     @Override
