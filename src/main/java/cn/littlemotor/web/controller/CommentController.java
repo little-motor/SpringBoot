9a5e0d2253562311adcc7a866e047612098d5536
littlemotor <479217391@qq.com>
2019-05-12 14:24:31 +0800

    已经基本实现评论的发送删除以及基本的交互功能，接下来进行进一步的完善和重构

--- src/main/java/cn/littlemotor/web/controller/CommentController.java
+++ src/main/java/cn/littlemotor/web/controller/CommentController.java
@@ -3,37 +3,34 @@ package cn.littlemotor.web.controller;
 import cn.littlemotor.web.model.dao.CommentDao;
 import cn.littlemotor.web.model.service.content.Comment;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 用于处理用户的评论
  * @author littlemotor
  * @date 19.5.6
  */
-@Controller
+@RestController
 public class CommentController {
 
     @Autowired
     CommentDao commentDao = null;
 
     //插入用户发表的评论
-    @PostMapping(path = "/comment")
-    public void insertComment(@RequestBody Comment comment){
+    @PostMapping(path = "/message/comment")
+    public int insertComment(@RequestBody Comment comment){
         commentDao.insertComment(comment);
+        return comment.getCommentId();
     }
 
     //删除评论
-    @DeleteMapping(path = "/comment")
+    @DeleteMapping(path = "/message/comment")
     public void deleteComment(int id){
         commentDao.deleteComment(id);
         System.out.println("delete id:" + id);
     }
 
-    @GetMapping(path = "/comments")
+    @GetMapping(path = "/message/comment")
     public void getComments(){
 
     }
