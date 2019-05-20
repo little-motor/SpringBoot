9c95a22aa5ce6429eaefb4be7d1ed61ca058ce5d
littlemotor <479217391@qq.com>
2019-05-20 23:16:42 +0800

    userDetailsServiceImpl结构进行了优化

--- src/main/java/cn/littlemotor/web/security/UserDetailsServiceImpl.java
+++ src/main/java/cn/littlemotor/web/security/UserDetailsServiceImpl.java
@@ -4,21 +4,18 @@ import cn.littlemotor.web.model.dao.UserDao;
 import cn.littlemotor.web.model.service.user.User;
 import cn.littlemotor.web.model.service.user.UserLogin;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.Collections;
 
 /**
  * 自定义用户认证方式
  * @author littlemotor
- * @date 19.4.7w
+ * @date 19.4.7
  */
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
@@ -26,60 +23,51 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Autowired
     UserDao userDao = null;
 
-    private UserLogin user = null;
+    private UserLogin userLogin = null;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        this.user = userDao.getUserbyEmail(email);
-        System.out.println(user.toMap());
-        return UserToUserdetails(user);
+        this.userLogin = userDao.getUserbyEmail(email);
+        System.out.println(userLogin.toMap());
+        return UserToUserdetails(userLogin);
     }
 
     //将user信息转换为userDetails对象
-    private UserDetails UserToUserdetails(UserLogin user){
-        String userName = user.getEmail();
-        String password = user.getPassword();
-        boolean enabled = (user.getActive() > 0);
-        //账户是否过期
-        boolean accountNonExpired = true;
-        //证书是否过期
-        boolean credentialsNonExpired = true;
-        boolean accountNonLocked = true;
-
-        List<GrantedAuthority> authorityList = new ArrayList<>();
-        authorityList.add(new SimpleGrantedAuthority(user.getRoleContent()));
+    private UserDetails UserToUserdetails(UserLogin userLogin){
 
         UserDetails userDetails =
-                new CustomUser(userName, password, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked, authorityList, user);
+                new CustomUser(userLogin);
         return  userDetails;
     }
 
     public User getUser() {
-        return user;
+        return userLogin;
     }
 
     /**
-     * 由于原来的User类记录的信息不是很全面，所以扩展之后加入了UserLogin实例，便于查看登陆用户信息
+     * 由于原来的User类记录的信息不是很全面，扩展后加入了UserLogin实例，便于查看登陆用户信息
      * CuntomUser实例在登陆验证后存在session内，key为"SPRING_SECURITY_CONTEXT"，
      * 返回类型为SecurityContextImpl,通过getAuthentication得到UsernamePasswordAuthenticationToken实例
      * 内部的princilp即为CustomUser
      */
     public class CustomUser extends org.springframework.security.core.userdetails.User{
 
+        private static final long serialVersionUID = -7958264758047805588L;
+        
         private UserLogin userLogin = null;
 
-        public CustomUser(String username, String password, Collection<? extends GrantedAuthority> authorities) {
-            super(username, password, authorities);
-        }
-
-        public CustomUser(String username, String password, boolean enabled, boolean accountNonExpired, boolean credentialsNonExpired, boolean accountNonLocked, Collection<? extends GrantedAuthority> authorities) {
-            super(username, password, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked, authorities);
-        }
-
-        public CustomUser(String username, String password, boolean enabled, boolean accountNonExpired, boolean credentialsNonExpired, boolean accountNonLocked, Collection<? extends GrantedAuthority> authorities, UserLogin userLogin) {
-            super(username, password, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked, authorities);
+        public CustomUser(UserLogin userLogin) {
+            super(userLogin.getEmail(),       //用户邮箱
+                    userLogin.getPassword(),    //密码
+                    userLogin.getActive() > 0,//账户是否有效
+                    true,       //accountNonExpired,
+                    true,     //credentialsNonExpired,
+                    true,        //accountNonLocked,
+                    //collection<? extends GrantedAuthority> authorities)此处使用了collections方法，因为直接用list add方法返回的是boolean
+                    Collections.nCopies(1,new SimpleGrantedAuthority(userLogin.getRole())));
             this.userLogin = userLogin;
         }
+
 
         public UserLogin getUserLogin() {
             return userLogin;
